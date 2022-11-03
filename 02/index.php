e54f88c48443d99b7faea25355477fee60e76935
Bishal Hona <mabishalhona@gmial.com>
2022-11-03 17:36:59 +0545

    Code refactor in assessment 2

--- 02/index.php
+++ 02/index.php
@@ -1,15 +1,16 @@
 <?php
 function floorNumbers(float $float, int $precision)
 {
-  $split = explode(".", $float);
-  $split[1] = substr_replace($split[1], ".", $precision, 0);
+  $decimal_point = ".";
+  $split = explode($decimal_point, $float);
+  $split[1] = substr_replace($split[1], $decimal_point, $precision, 0);
 
   if ($split[0] >= 0) {
     $split[1] = floor($split[1]);
   } else {
     $split[1] = ceil($split[1]);
   }
-  $output = $split[0] . "." . $split[1];
+  $output = $split[0] . $decimal_point . $split[1];
   return $output;
 }
 echo floorNumbers(-1.45499, 4) . "</br>";
